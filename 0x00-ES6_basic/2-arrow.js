600fbd847dc3ef4a0b6f96d14de1be9adb99b304
samsonkimani <samsonkimani43@gmail.com>
2023-06-27 11:12:12 +0300

    making the add function an arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,19 @@
+// export default function getNeighborhoodsList() {
+//   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+//   const self = this;
+//   this.addNeighborhood = function add(newNeighborhood) {
+//     self.sanFranciscoNeighborhoods.push(newNeighborhood);
+//     return self.sanFranciscoNeighborhoods;
+//   };
+// }
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
