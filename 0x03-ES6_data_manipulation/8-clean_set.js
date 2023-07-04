57238653c879b7551515a9a31e7d248959656869
samsonkimani <samsonkimani43@gmail.com>
2023-07-04 19:33:07 +0300

    a function to use substring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
+  if (startString.length === 0 || startString === undefined) {
     return '';
   }
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
