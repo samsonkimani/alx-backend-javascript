b0870456e8bb1f7cfb6dff79d954a72938bef921
samsonkimani <samsonkimani43@gmail.com>
2023-07-05 18:28:05 +0300

    extending interfaces

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,3 +1,11 @@
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
 var teacher3 = {
     firstName: 'John',
     fullTimeEmployee: false,
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,20 @@ interface Teacher {
 	[key: string ]: any;
 }
 
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
