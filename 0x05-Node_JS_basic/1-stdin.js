07b2862c94bbaefa073083888542b7ae9bd431aa
samsonkimani <samsonkimani43@gmail.com>
2023-08-22 14:14:51 +0300

    process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
