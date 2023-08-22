dc89dd0b88dcbc91f2807aeb6491683c211bb481
samsonkimani <samsonkimani43@gmail.com>
2023-08-22 21:32:12 +0300

    creating a server and adding routes

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const port = 1245;
+const hostname = '127.0.0.1';
+const countStudents = require('./3-read_file_async.js');
+
+const databasePath = process.argv[2];
+
+const app = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	if (req.url === '/') {
+		res.write('Hello Holberton School!');
+		res.end();
+	} else if (req.url === '/students') {
+		res.write('This is the list of our students');
+		countStudents(databasePath)
+		.then((output) => {
+			res.end(output);
+		})
+		.catch((error) => {
+			res.writeHead(500, { 'Content-Type': 'text/plain' });
+			res.end(error.message);
+		});
+	}else {
+		res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.end('Not Found\n');
+	}
+});
+
+app.listen(port, hostname, () => {});
+
+module.exports = app;
