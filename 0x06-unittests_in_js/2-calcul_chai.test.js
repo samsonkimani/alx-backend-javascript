fb5cf2869bf87ce00b722fc9f6d688175e85fe88
samsonkimani <samsonkimani43@gmail.com>
2023-08-24 15:05:50 +0300

    using chai for unit testing

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+	it('should work based on descriptions', () => {
+		expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+		expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+		expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+		expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+	});
+});
